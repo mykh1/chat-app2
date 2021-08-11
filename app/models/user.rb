3b10b5cdafae61a847ae28d6a52becf2b5cc7a97
mykh1 <neige391124@yahoo.co.jp>
2021-08-11 20:20:11 +0900

    チャット画面の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   has_many :room_users
   # userは１対多でroom_usersと繋がる
-  has_many :users, through: :room_users
+  has_many :rooms, through: :room_users
   # userはスルーオプションでroom_usersを経由してroomと繋がる
 
 end
