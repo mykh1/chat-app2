175745bef67979434861f94fb3cb37c84263aac1
mykh1 <neige391124@yahoo.co.jp>
2021-08-12 17:17:00 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,14 @@ class Message < ApplicationRecord
   # has_one_attachedハズ ワン アタッチドメソッド
   # 上記を記述したモデルの各レコードは、それぞれ1つのファイルを添付できる
 
-  validates :content, presence: true
-  # バリデーション。「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しない設定
+  validates :content, presence: true, unless: :was_attached?
+  # バリデーション。「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しない設定。メッセージと画像のどちらにも適応される。
+  # 
+  # unlessオプションにメソッド名was_attached?を指定することで、imageを持っている時はtrueで、imageが投稿され、falseだとメッセージが投稿される
+
+  def was_attached? #メソッド名は自分で決める。
+    self.image.attached?
+    # imageを添付しているか？持ってるか？という意味。
+  end
+
 end
