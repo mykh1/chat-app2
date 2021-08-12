b4ad9119c23408f5925e5c706edc8af35e27d4fe
mykh1 <neige391124@yahoo.co.jp>
2021-08-12 15:53:52 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,9 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   # roomは１対多でroom_usersと繋がる
   has_many :users, through: :room_users
   # roomはスルーオプションでroom_usersを経由してuserと繋がる
-  has_many :messages
+  has_many :messages, dependent: :destroy
   # roomは１対多でmessagesと繋がる  
   # 1つのチャットルームに、メッセージは複数存在する
   validates :name, presence: true
