67c88aa7d2ebaf04be4e4e861beec1886de3ac12
mykh1 <neige391124@yahoo.co.jp>
2021-08-12 11:46:07 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,11 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+    # ユーザーの情報を含んだ（紐づいた）メッセージを取り出す。
+    # 一人のユーザーがした複数のメッセージを取り出す。
+    # includesメソッド.N+1問題.
+    # 全てのメッセージ情報に紐づくユーザー情報を、includes(:user)と記述をすることにより、ユーザー情報を1度のアクセスでまとめて取得
   end
 
   def create
@@ -10,6 +15,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
