99cc994244100084c2147896d9ae82ba86366492
mykh1 <neige391124@yahoo.co.jp>
2021-08-12 10:28:40 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+    # ネストをすることにより、roomsが親で、messagesが子という親子関係
+    # チャットルームに属しているメッセージという意味
+  end
+  
 end
