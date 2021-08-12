50c1273773d783deb7b74def1b49f89b5c18df3a
mykh1 <neige391124@yahoo.co.jp>
2021-08-12 16:25:14 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,9 @@ class Message < ApplicationRecord
   # 1つのメッセージは、1つのチャットルームに存在する。
   belongs_to :user
   # 1つのメッセージは、1人のユーザーから送信される。
+  hax_one_attached :image
+  # has_one_attachedハズ ワン アタッチドメソッド
+  # 上記を記述したモデルの各レコードは、それぞれ1つのファイルを添付できる
 
   validates :content, presence: true
   # バリデーション。「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しない設定
